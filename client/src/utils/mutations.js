454028f610e16bfed0e5dcdc71a16e942778379c
Asher <asher.bergtraun@gmail.com>
2021-10-01 15:21:42 -0700

    completed add user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,20 +25,23 @@ export const LOGIN_USER  = gql`
 `;
 //export add user mutation for GraphQL
 export const ADD_USER  = gql`
-    {
-        me {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
             _id
             username
             email
             bookCount
             savedBooks {
-                bookId
-                authors
-                title
-                description
-                image
-                link
+            bookId
+            title
+            description
+            authors
+            image
+            link
             }
+        }
         }
     }
 `;
