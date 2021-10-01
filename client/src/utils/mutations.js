97f4da981cce672a0655f21e9b14ff75687aad08
Asher <asher.bergtraun@gmail.com>
2021-10-01 15:20:07 -0700

    listed all 4 mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,65 @@
 //import GraphQL
 import gql from "graphql-tag";
 
-//export queries for GraphQL
-export const GET_ME  = gql`
+//export login mutation for GraphQL
+export const LOGIN_USER  = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+//export add user mutation for GraphQL
+export const ADD_USER  = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+//export save book mutation for GraphQL
+export const SAVE_BOOK  = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+//export remove book mutation for GraphQL
+export const REMOVE_BOOK  = gql`
     {
         me {
             _id
