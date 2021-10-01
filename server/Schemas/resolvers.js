4294a0d8ad2032af39a4b8a76c9b03c4f715c103
Asher <asher.bergtraun@gmail.com>
2021-10-01 14:09:01 -0700

    built query resolver  object

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -3,3 +3,17 @@ const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
+//resolvers
+const resolvers = {
+    //Query resolver object
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not Logged In.');
+        }
+    }
+}
