428dde88ee2ed9d3712b48c5fbdb2dd2ad6983ec
Asher <asher.bergtraun@gmail.com>
2021-10-01 13:58:03 -0700

    completed front end schema/index.js to connect to typeDefs and resolvers

---
+++ server/Schemas/index.js
@@ -0,0 +1,6 @@
+//require connection to typeDefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+//export typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
